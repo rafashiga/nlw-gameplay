c4febf89579267e88809ac3ee6c7cf3abdaf4569
Rafael <rafa.shiga@gmail.com>
2021-06-24 14:30:55 -0300

    refactor(profile): add user info from discord data

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useAuth } from '../../hooks/auth';
 
 import { Avatar } from '../Avatar';
 
 import { styles } from './styles';
 
 export const Profile = () => {
+	const { user } = useAuth();
+
 	return (
 		<View style={styles.container}>
-			<Avatar urlImage='https://images.unsplash.com/profile-1608037205070-e47e53995a43image?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff' />
+			<Avatar urlImage={user.avatar} />
 			<View>
 				<View style={styles.user}>
 					<Text style={styles.greeting}>Olá,</Text>
-					<Text style={styles.username}>Rafael</Text>
+					<Text style={styles.username}>{user.firstName}</Text>
 				</View>
 				<Text style={styles.message}>Hoje é dia de vitória</Text>
 			</View>
