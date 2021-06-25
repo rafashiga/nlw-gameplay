1aff5a44ee22c18649b7155ddc4f15d2d6ba99a4
Rafael <rafa.shiga@gmail.com>
2021-06-25 11:40:57 -0300

    feat: save appointment form data

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -6,6 +6,9 @@ import {
 	KeyboardAvoidingView,
 	Platform,
 } from 'react-native';
+import uuid from 'react-native-uuid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 
@@ -22,6 +25,7 @@ import {
 } from '../../components';
 
 import { Guilds } from '../Guilds';
+import { COLLECTION_APPOINTMENTS } from '../../configs';
 
 import { styles } from './styles';
 import { theme } from '../../global/styles/theme';
@@ -30,6 +34,14 @@ export const AppointmentCreate = () => {
 	const [category, setCategory] = useState('');
 	const [openGuildsModal, setOpenGuildsModal] = useState(false);
 	const [guild, setGuild] = useState<IGuild>({} as IGuild);
+
+	const [day, setDay] = useState('');
+	const [month, setMonth] = useState('');
+	const [hour, setHour] = useState('');
+	const [minute, setMinute] = useState('');
+	const [description, setDescription] = useState('');
+
+	const navigation = useNavigation();
 
 	const handleOpenGuildsModal = () => {
 		setOpenGuildsModal(true);
@@ -46,6 +58,26 @@ export const AppointmentCreate = () => {
 
 	const handleCategorySelect = (categoryId: string) => {
 		setCategory(categoryId);
+	};
+
+	const handleSave = async () => {
+		const newAppointment = {
+			id: uuid.v4(),
+			guild,
+			category,
+			date: `${day}/${month} às ${hour}:${minute}h`,
+			description,
+		};
+
+		const storage = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+		const appointments = storage ? JSON.parse(storage) : [];
+
+		await AsyncStorage.setItem(
+			COLLECTION_APPOINTMENTS,
+			JSON.stringify([...appointments, newAppointment])
+		);
+
+		navigation.navigate('Home');
 	};
 
 	return (
@@ -97,9 +129,9 @@ export const AppointmentCreate = () => {
 								</Text>
 
 								<View style={styles.column}>
-									<SmallInput maxLength={2} />
+									<SmallInput maxLength={2} onChangeText={setDay} />
 									<Text style={styles.divider}>/</Text>
-									<SmallInput maxLength={2} />
+									<SmallInput maxLength={2} onChangeText={setMonth} />
 								</View>
 							</View>
 
@@ -109,9 +141,9 @@ export const AppointmentCreate = () => {
 								</Text>
 
 								<View style={styles.column}>
-									<SmallInput maxLength={2} />
+									<SmallInput maxLength={2} onChangeText={setHour} />
 									<Text style={styles.divider}>:</Text>
-									<SmallInput maxLength={2} />
+									<SmallInput maxLength={2} onChangeText={setMinute} />
 								</View>
 							</View>
 						</View>
@@ -124,9 +156,10 @@ export const AppointmentCreate = () => {
 							maxLength={100}
 							numberOfLines={5}
 							autoCorrect={false}
+							onChangeText={setDescription}
 						/>
 						<View style={styles.footer}>
-							<Button title='Agendar' />
+							<Button title='Agendar' onPress={handleSave} />
 						</View>
 					</View>
 				</ScrollView>
